1b33b28f61b4affbe86cce5dc69b4a9b575bc490
Muhammad Ismail <62374544+ismail-se@users.noreply.github.com>
2021-01-05 15:27:43 +0500

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -5,13 +5,20 @@ var canvas = document.querySelector("#canvas");
 var context = canvas.getContext("2d");
 
 var image = document.querySelector("img");
-console.log(image.width)
+console.log(image.width);
 canvas.width = image.width;
 canvas.height = image.height;
 
-var color = "#ff0000"
+var color = document.querySelector("#color");
+var time = document.querySelector("#duration");
+
+let interval = "";
 
 const createSnake = () => {
+  if (interval !== "") {
+    clearInterval(interval);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  }
   let tl = 0,
     tr = 0,
     br = 1,
@@ -23,54 +30,50 @@ const createSnake = () => {
     topRight = 0,
     topLeft = 0,
     count = 0;
-    
-    context.fillStyle= color;
-  let interval = setInterval(() => {
-      if (count === 1){
-          topRight++;
-          topLeft++;
-          width--;
-          height--;
-          tr = topRight;
-          tl = topLeft;
-          br = topRight+1;
-          bl = topLeft+1;
-          console.log("count " , count%4)
-          count = 0;
-      }
+
+  context.fillStyle = color.value;
+  interval = setInterval(() => {
+    if (count === 1) {
+      topRight++;
+      topLeft++;
+      width--;
+      height--;
+      tr = topRight;
+      tl = topLeft;
+      br = topRight + 1;
+      bl = topLeft + 1;
+      console.log("count ", count % 4);
+      count = 0;
+    }
     if (br < width && tr == topRight && tl == topLeft) {
-        console.log("first")
+      console.log("first");
       context.rect(tl, tr, br, bl);
       context.fillRect(tl, tr, br, bl);
       br++;
-    } else if (bl < height && br >= width-1) {
-      tl = width-1;
+    } else if (bl < height && br >= width - 1) {
+      tl = width - 1;
       tr = 0;
-      console.log("second")
+      console.log("second");
       context.rect(tl, tr, br, bl);
       context.fillRect(tl, tr, br, bl);
       bl++;
-   
-    } else if (bl > height-1 && tl > topLeft) {
-      tr = height-1;
-      console.log("third")
+    } else if (bl > height - 1 && tl > topLeft) {
+      tr = height - 1;
+      console.log("third");
       context.rect(tl, tr, br, bl);
       context.fillRect(tl, tr, br, bl);
       tl--;
-      
     } else if (tr > topRight) {
-        br=topRight+1;
-        bl=topLeft+1;
-        tl=topLeft;
-        console.log("last")
+      br = topRight + 1;
+      bl = topLeft + 1;
+      tl = topLeft;
+      console.log("last");
       context.rect(tl, tr, br, bl);
       context.fillRect(tl, tr, br, bl);
       tr--;
-      if (tr === topRight+1){
-          count = 1;
+      if (tr === topRight + 1) {
+        count = 1;
       }
     }
-    
-  }, 0.5);
+  }, time);
 };
-createSnake();
